475337dcac57dabc8f0cf2ca2d3f7e5b4b303952
Carlton Hanna <nullpointer0x00@gmail.com>
2017-07-05 06:59:08 -0600

    Add cache weather history files for now

--- sql/rollups-queries.sql
+++ sql/rollups-queries.sql
@@ -9,6 +9,7 @@ sfs.lat as latitude,
 sfs.long as longitude,
 sfs.landmark,
 sfs.installation
+concat(YEAR(ss.time), '-', MONTH(ss.time), '-', DAY(ss.time)) as date_str
 FROM `sfbike`.`station_status` as ss
 INNER JOIN `sfbike`.`station` as sfs
 ON ss.station_id = sfs.station_id
@@ -20,4 +21,12 @@ day(ss.time),
 month(ss.time),
 hour(ss.time),
 ss.bikes_available
+order by time asc;
+
+CREATE TABLE `sfbike`.`sf_station_activity_weather` SELECT
+sfsa.*,
+ws.*
+FROM `sfbike`.`sf_station_activity` as sfsa
+left Join `sfbike`.`weather_summary` as ws
+on concat(YEAR(ws.date), '-', MONTH(ws.date), '-', DAY(ws.date)) = concat(YEAR(sfsa.time), '-', MONTH(sfsa.time), '-', DAY(sfsa.time))
 order by time asc;
